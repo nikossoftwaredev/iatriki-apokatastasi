aadb1f5d63de7b373acae2325bff71d71e29e8f6
Nikos Dim <ndimitrakopoulos@maseurope.com>
2022-11-28 14:00:16 +0200

    added banner temp css fix

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 cloning this site [https://design1.doctorsitecare.com/]
+
+or this [https://www.skinhair.gr/]
--- ui/Banner.tsx
+++ ui/Banner.tsx
@@ -1,31 +1,41 @@
+import { servicesInfo } from 'data/services';
 import React from 'react';
-import { MdAccessibility } from 'react-icons/md';
-import { MdFace } from 'react-icons/md';
+import { MdVerifiedUser, MdMedicalServices, MdSentimentVerySatisfied } from 'react-icons/md';
 import { calculateYears } from 'utils/year';
 
 const items = [
   {
     id: 1,
-    icon: <MdAccessibility size={70} />,
+    icon: <MdVerifiedUser size={80} />,
     number: calculateYears(),
-    description: 'years of experience'
+    description: 'Xρονια λειτουργιας'
   },
-  { id: 2, icon: <MdFace size={70} />, number: 2021, description: 'best of point vedra' },
-  { id: 3, icon: <MdAccessibility size={70} />, number: '200+', description: 'happy customers' }
+  {
+    id: 2,
+    icon: <MdMedicalServices size={80} />,
+    number: servicesInfo.length,
+    description: 'εξειδικευμενες υπηρεσιες'
+  },
+  {
+    id: 3,
+    icon: <MdSentimentVerySatisfied size={80} />,
+    number: '2000+',
+    description: 'Ευχαριστημενοι πελατες'
+  }
 ];
 
 const Banner = () => {
   return (
-    <div className='relative '>
-      <div className='w-full  absolute flex flex-col items-center justify-center xl:-top-[125px] md:-top-[40px]'>
-        <div className='xs:4/5 sm:w-4/5 md:w-4/5 lg:3/4 xl:w-2/3 shadow-lg bg-blue p-14 text-white rounded-lg small'>
+    <div className='relative'>
+      <div className='w-full flex flex-col items-center justify-center xl:-top-[125px] md:-top-[40px]'>
+        <div className='xs:4/5 sm:w-4/5 md:w-4/5 lg:4/5 xl:w-2/3 shadow-lg bg-blue p-14 text-white rounded-lg small'>
           <section className='grid xl:grid-flow-col xl:justify-evenly md:row-span-1 md:grid-flow-row md:items-center md:justify-center '>
             {items.map(item => (
-              <div key={item.id} className='grid grid-cols-3 md:my-4'>
-                <div className='flex justify-center row-span-3 col-span-1'>{item.icon}</div>
-                <div className='text-4xl font-bold row-span-1 col-span-2'>{item.number}</div>
-                <div className='text-lg py-6  row-span-1 col-span-2'>
-                  {item.description.toUpperCase()}
+              <div key={item.id} className='flex gap-5 md:my-4 items-center justify-center'>
+                {item.icon}
+                <div className='flex justify-start items-start flex-col'>
+                  <div className='text-4xl font-extrabold'>{item.number}</div>
+                  <div className='text-lg py-6'>{item.description.toUpperCase()}</div>
                 </div>
               </div>
             ))}
