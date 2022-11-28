0d204bec8d9024096d02c48879248fe1275e32ef
Nikos Dim <ndimitrakopoulos@maseurope.com>
2022-11-28 11:24:01 +0200

    removed fragments

--- ui/Banner.tsx
+++ ui/Banner.tsx
@@ -20,19 +20,15 @@ const Banner = () => {
       <div className='w-full  absolute flex flex-col items-center justify-center -top-[125px]'>
         <div className='xs:4/5 sm:w-4/5 md:w-4/5 lg:3/4 xl:w-3/4 shadow-lg bg-blue p-16 text-white rounded-lg small'>
           <section className='grid xl:grid-flow-col xl:justify-evenly md:row-span-1 md:grid-flow-row md:items-center md:justify-center '>
-            {items.map(item => {
-              return (
-                <>
-                  <div key={item.id} className='grid grid-cols-3 md:my-4'>
-                    <div className='flex justify-center row-span-3 col-span-1'>{item.icon}</div>
-                    <div className='text-4xl font-bold row-span-1 col-span-2'>{item.number}</div>
-                    <div className='text-lg py-6  row-span-1 col-span-2'>
-                      {item.description.toUpperCase()}
-                    </div>
-                  </div>
-                </>
-              );
-            })}
+            {items.map(item => (
+              <div key={item.id} className='grid grid-cols-3 md:my-4'>
+                <div className='flex justify-center row-span-3 col-span-1'>{item.icon}</div>
+                <div className='text-4xl font-bold row-span-1 col-span-2'>{item.number}</div>
+                <div className='text-lg py-6  row-span-1 col-span-2'>
+                  {item.description.toUpperCase()}
+                </div>
+              </div>
+            ))}
           </section>
         </div>
       </div>
