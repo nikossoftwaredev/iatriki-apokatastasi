7907c7e1403e2325617b694fb4d7e28b386455ad
Nikos Dim <ndimitrakopoulos@maseurope.com>
2022-11-28 14:31:05 +0200

    oti nanai

--- ui/Banner.tsx
+++ ui/Banner.tsx
@@ -26,21 +26,22 @@ const items = [
 
 const Banner = () => {
   return (
-    <div className='relative'>
-      <div className='w-full flex flex-col items-center justify-center xl:-top-[125px] md:-top-[40px]'>
-        <div className='xs:4/5 sm:w-4/5 md:w-4/5 lg:4/5 xl:w-2/3 shadow-lg bg-blue p-14 text-white rounded-lg small'>
-          <section className='grid xl:grid-flow-col xl:justify-evenly md:row-span-1 md:grid-flow-row md:items-center md:justify-center '>
-            {items.map(item => (
-              <div key={item.id} className='flex gap-5 md:my-4 items-center justify-center'>
-                {item.icon}
-                <div className='flex justify-start items-start flex-col'>
-                  <div className='text-4xl font-extrabold'>{item.number}</div>
-                  <div className='text-lg py-6'>{item.description.toUpperCase()}</div>
-                </div>
+    <div className={`w-full flex flex-col items-center justify-center relative `}>
+      <div className='xs:4/5 sm:w-4/5 md:w-4/5 lg:4/5 xl:w-2/3 shadow-lg bg-blue p-14 text-white rounded-lg small'>
+        <section className='grid xl:grid-flow-col xl:justify-evenly md:row-span-1 md:grid-flow-row md:items-start md:justify-start '>
+          {items.map(item => (
+            <div
+              key={item.id}
+              className='flex gap-5 md:my-4 items-start justify-center md:items-start md:justify-start'
+            >
+              {item.icon}
+              <div className='flex justify-start items-start flex-col gap-2'>
+                <div className='text-4xl font-extrabold'>{item.number}</div>
+                <div className='text-lg '>{item.description.toUpperCase()}</div>
               </div>
-            ))}
-          </section>
-        </div>
+            </div>
+          ))}
+        </section>
       </div>
     </div>
   );
